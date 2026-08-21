ad995ebaafa62feda62f5a95edd681ce28f95b73
hopesain <hopessain@gmail.com>
2026-08-21 12:08:53 +0200

    get current user information

---
+++ internal/jira/myself/myself.go
@@ -0,0 +1,53 @@
+package myself
+
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/hopesain/gojira/internal/jira"
+)
+
+func GetCurrentUser(credentials jira.JiraCredentials) error {
+	client := &http.Client{
+		Timeout: time.Second * 15,
+	}
+
+	url := credentials.BaseUrl + "/myself"
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return fmt.Errorf("failed to build the request: %w", err)
+	}
+
+	req.SetBasicAuth(credentials.Email, credentials.Token)
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("request failed: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read the request body: %w", err)
+	}
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		return fmt.Errorf("incorrect authorization credentials. check your email and token")
+	}
+
+	slog.Info("jira request completed",
+		"path", url,
+		"status", resp.Status,
+		"status_code", resp.StatusCode,
+		"body", string(body),
+	)
+
+	return nil
+
+}
